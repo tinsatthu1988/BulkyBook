29c8a977a73e570e8590ba15e4106c92fd87bf80
TIN-THIET-KE <TIN-THIET-KE@TIN-THIET-KE>
2021-01-05 10:29:16 +0700

    15. Models and Repository

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -22,5 +22,8 @@ namespace BulkyBook.DataAcess.Data
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
         public DbSet<Company> Companies { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
     }
 }
